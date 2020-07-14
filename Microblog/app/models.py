a5a2c66aa9d7c3016c266f4c6b51605aa4297ca8
Bharat-Runwal <ee3180598@iitd.ac.in>
2020-07-14 14:07:19 +0530

    first after long time

--- Microblog/app/models.py
+++ Microblog/app/models.py
@@ -5,7 +5,6 @@ from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
 
 
-
 class User(UserMixin,db.Model):
     id =db.Column(db.Integer,primary_key = True)
     username= db.Column(db.String(64), index=True, unique =True)
--- Microblog/config.py
+++ Microblog/config.py
@@ -1,5 +1,6 @@
 import os 
 base_dir = os.path.abspath(os.path.dirname(__file__))
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')or \
