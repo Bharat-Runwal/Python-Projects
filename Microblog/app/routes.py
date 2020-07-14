0ca1126263f0d608281d3ed327df8e16cdf8f579
Bharat-Runwal <ee3180598@iitd.ac.in>
2020-07-14 16:48:42 +0530

    Profile of User

--- Microblog/app/routes.py
+++ Microblog/app/routes.py
@@ -66,4 +66,14 @@ def register():
         return (redirect('/index'))
         
     return render_template('register.html',title='Register',form=form)
-    
+
+@app.route('/user/<username>')
+@login_required
+
+def user(username):
+    user =User.query.filter_by(username=username).first_or_404()
+    posts= [
+        {'author':user ,'body':'Text post#1'},
+        {'author':user ,'body':'Text post#2'}
+    ]
+    return render_template('user.html',user = user,posts=posts)
