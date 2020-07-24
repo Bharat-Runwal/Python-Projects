ee08d0770e3bfb49c0e593d53397fe8c145b4eb4
Bharat-Runwal <ee3180598@iitd.ac.in>
2020-07-24 13:54:37 +0530

    Language translation

--- Microblog/app/__init__.py
+++ Microblog/app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask,request
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -9,6 +9,7 @@ from logging.handlers import SMTPHandler,RotatingFileHandler
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel, lazy_gettext as _l
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -17,9 +18,19 @@ db= SQLAlchemy(app)
 migrate = Migrate(app,db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
+
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel =Babel(app)
+
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(config['LANGUAGES'])
+
 if not app.debug:
     if app.config['MAIL_SERVER']:
         auth=None
--- Microblog/config.py
+++ Microblog/config.py
@@ -15,3 +15,5 @@ class Config(object):
     ADMINS = ['workrelated.bharat@gmail.com']
     
     POSTS_PER_PAGE=25
+
+    LANGUAGES = ['en','es']
