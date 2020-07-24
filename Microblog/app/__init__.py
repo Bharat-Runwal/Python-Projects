ced88b9d44db091018c978b7490060063c077bac
Bharat-Runwal <ee3180598@iitd.ac.in>
2020-07-24 14:14:14 +0530

    Translate Languages function added to app

--- Microblog/app/__init__.py
+++ Microblog/app/__init__.py
@@ -29,7 +29,7 @@ babel =Babel(app)
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(config['LANGUAGES'])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
